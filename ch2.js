6148baa06e1a70b37cce4e7b6c239da0d1ef1f2b
aurimas-darguzis <aurimas@darguzis.com>
2017-11-19 17:31:33 +0000

    update

--- ch2.js
+++ ch2.js
@@ -7,3 +7,25 @@ foo: ----0---1---2---3--...
 bar: ----0---2---4---6--...
 
 */
+
+function calculate (transformationFn) {
+  const source = this;
+  const result = Rx.Observable.create(function subscribe (observer) {
+    source.subscribe(
+      function (x) { observer.next(transformationFn(x)) },
+      function (err) { observer.error(err) },
+      function () { observer.complete() }
+    )
+  })
+  return result
+}
+
+Rx.Observable.prototype.calculate = calculate
+
+const bar = foo.calculate(x => x * 2)
+
+bar.subscribe(
+  function (x) { console.log(`next ${x}`)},
+  function (err) { console.log(`error ${err}`)},
+  function () { console.log('done')}
+)
