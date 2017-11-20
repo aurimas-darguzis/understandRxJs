27692a41e70d1bb0beb3a0ea7a4654da318f6194
Aurimas <aurimas@darguzis.com>
2017-11-20 09:02:25 +0000

    update

---
+++ ch3.js
@@ -0,0 +1,13 @@
+const foo = Rx.Observable.interval(200).take(4)
+
+// do is usefull for debugging purpose
+// it returns the value
+const bar = foo.do(function (x) {
+    console.log(`! ${x}`)
+})
+
+bar.subscribe(
+    function (x) { console.log(`next ${x}`)},
+    function (err) { console.log(`error ${err}`)},
+    function () { console.log('done')}
+)
