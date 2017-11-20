fe434ed82249adc09697c1f14f5b0397b3221c41
Aurimas <aurimas@darguzis.com>
2017-11-20 09:03:53 +0000

    update

--- ch3.js
+++ ch3.js
@@ -2,6 +2,7 @@ const foo = Rx.Observable.interval(200).take(4)
 
 // do is usefull for debugging purpose
 // it returns the value
+// do does not invokes subscription
 const bar = foo.do(function (x) {
     console.log(`! ${x}`)
 })
