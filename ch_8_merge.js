1caa376b26514322eac351f2479db1e77d47907d
Aurimas <aurimas@darguzis.com>
2017-11-21 12:56:26 +0000

    update

--- ch_8_merge.js
+++ ch_8_merge.js
@@ -1,4 +1,18 @@
 const foo = Rx.Observable.interval(500).take(4)
 const bar = Rx.Observable.interval(300).take(5)
 
+/**
+ * ----0----1----2----(3|)  foo()
+ * --0--1--2--3--(4|)       bar()
+ *     merge (OR operator)
+ * --0-01--21-3--(24|)-(3|)
+ */
+
+
 const merged = Rx.Observable.merge(foo, bar)
+
+merged.subscribe(
+    function (x) { console.log(`next: ${x}`)},
+    function (err) { console.log(`Error: ${err}`)},
+    function () { console.log('done')}
+)
