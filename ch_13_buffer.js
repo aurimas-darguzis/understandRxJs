c626623a0cdd407e8eef12eabdf860c21d1f83e1
aurimas-darguzis <aurimas@darguzis.com>
2017-11-25 23:42:32 +0000

    update

---
+++ ch_13_buffer.js
@@ -0,0 +1,24 @@
+const foo = Rx.Observable.of('h', 'e', 'l', 'l', 'o')
+    .zip(Rx.Observable.interval(600).take(5), (x,y) => x)
+
+/**
+ * -----h-----e-----l-----l-----o    (foo)
+ *  bufferCount(2)
+ * -----------he-----------ll------o|
+ * 
+ *    buffer
+ *    bufferCount
+ *    bufferTime
+ *    bufferToggle
+ *    bufferWhen
+ * 
+ * -----h-----(he)--(hel)-(hell)(hello|)
+ */
+
+ const result = foo.bufferCount(2)
+
+ result.subscribe(
+   function (x) { console.log(`next ${x}`)},
+   function (err) { console.log(`error ${err}`)},
+   function () { console.log('done')}
+ )
