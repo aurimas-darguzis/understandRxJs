4c7b9d8d2c96d0f76fbe82672b9f83782e359c6b
aurimas-darguzis <aurimas@darguzis.com>
2017-11-26 00:34:09 +0000

    update

---
+++ ch_19_retry.js
@@ -0,0 +1,20 @@
+const foo = Rx.Observable.interval(500)
+  .zip(Rx.Observable.of('a','b','c','d',2), (x, y )=> y)
+
+const bar = foo.map(x => x.toUpperCase())
+
+/**
+ * --a--b--c--d--2| (foo)
+ * map(toUpperCase)
+ * --A-B--C--D--#   (bar)
+ *  retry
+ * --A-B--C--D------A-B--C--D----A-B--C--D-#
+ */
+
+const result = bar.retry(2)
+
+result.subcribe(
+  function (x) { console.log(`next ${x}`)},
+  function (err) { console.log(`error ${err}`)},
+  function () { console.log('done')}
+)
