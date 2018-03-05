cf8b502a48fae8cb42dd9550c1af8f0294251451
Aurimas <aurimas@darguzis.com>
2018-03-05 21:22:31 +0000

    update

---
+++ reactive programming/ch2_doubleClick.js
@@ -0,0 +1,19 @@
+const button = document.querySelector('.button');
+const label = document.querySelector('h4');
+
+const clickStream = Rx.Observable.fromEvent(button, 'click');
+
+const doubleClickStream = clickStream
+    .bufferWhen(() => clickStream.debounceTime(250))
+    .map(arr => arr.length)
+    .filter(len => len === 2);
+
+doubleClickStream.subscribe(event => {
+    label.textContent = 'double click';
+});
+
+doubleClickStream
+    .delay(1000)
+    .subscribe(suggestion => {
+        label.textContent = '-';
+    });
